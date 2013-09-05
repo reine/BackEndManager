c33d9b518c7a8e6e01af3d23ac8f546ccfe5465b
Reine Sison <reawakened.phoenix@gmail.com>
2013-09-05 14:52:06 +0800

    Initial commit on revamped version using 'one core-multiple apps' CI-BEP system.

---
+++ apps/scm/logs/log-2013-09-05.php
@@ -0,0 +1,11 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2013-09-05 06:49:11 --> Could not find the language line "preference_label_allow_user_registration"
+ERROR - 2013-09-05 06:49:11 --> Could not find the language line "preference_label_activation_method"
+ERROR - 2013-09-05 06:49:11 --> Could not find the language line "preference_label_account_activation_time"
+ERROR - 2013-09-05 06:49:11 --> Could not find the language line "preference_label_autologin_period"
+ERROR - 2013-09-05 06:49:11 --> Could not find the language line "preference_label_default_user_group"
+ERROR - 2013-09-05 06:49:11 --> Could not find the language line "preference_label_allow_user_profiles"
+ERROR - 2013-09-05 06:49:11 --> Could not find the language line "preference_label_login_field"
+ERROR - 2013-09-05 06:49:11 --> Could not find the language line "preference_label_min_password_length"
+ERROR - 2013-09-05 06:49:11 --> Could not find the language line "preference_label_email_protocol"
